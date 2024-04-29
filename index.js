e6bce0f2bc39c349ac966919206153f5e314ef96
Biplob Sarkar <ramjansarkar3899@gmail.com>
2024-04-29 22:19:27 +0600

    user create craft data display for logged in user

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@biplob.whidwsu.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,24 +22,52 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
+
+    // create database and data table
+    const addAllCrafts = client.db('artfulGlassAndPaper').collection('addCrafts');
+
+    // Get data from database to show data
+    app.get('/addCrafts', async (req, res) => {
+      try {
+        const cursor = addAllCrafts.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching crafts:", error);
+        res.status(500).json({ error: "Failed to fetch crafts" });
+      }
+    });
+    
+
+    app.post('/addCrafts', async(req, res) => {
+      try {
+        const newAddCraft = req.body;
+        const result = await addAllCrafts.insertOne(newAddCraft);
+        console.log("New craft added:", newAddCraft);
+        res.json(result);
+      } catch (error) {
+        console.error("Error adding craft:", error);
+        res.status(500).json({ error: "Failed to add craft" });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    // Ensure that the client will close when you finish/error
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-    res.send('server is running')
-})
+  res.send('Server is running');
+});
 
 app.listen(port, () => {
-    console.log(`server is running on port: ${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
