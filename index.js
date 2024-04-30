17c0389cd9ddd3f2a11fd3d957817aae87da1523
Biplob Sarkar <ramjansarkar3899@gmail.com>
2024-04-30 21:25:50 +0600

    db admin ping remove

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 }); //aita pore comment kore dite hobe
+    // await client.db("admin").command({ ping: 1 }); 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensure that the client will close when you finish/error
